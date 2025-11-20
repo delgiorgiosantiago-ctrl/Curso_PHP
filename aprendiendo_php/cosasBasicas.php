bc7d8c59aae9ae0f16e1862b2d7d213653e491ad
sdelgiorgio <sdelgiorgio@minfra.gba.gob.ar>
2025-11-20 14:03:13 -0300

    funciones

--- aprendiendo_php/cosasBasicas.php
+++ aprendiendo_php/cosasBasicas.php
@@ -423,7 +423,73 @@
         }
         echo "<hr/>";
 
+        // Ejemplo 3
+        function calculadora($numero1, $numero2, $negrita = false){
+            
+            $suma = $numero1 + $numero2;
+            $resta = $numero1 - $numero2;
+            $multiplicacion = $numero1 * $numero2;
+            $division = $numero1 / $numero2;
+
+            if($negrita){
+                echo "<h1>";
+            }
+
+            echo "<h3>Calculadora</h3>";
+            echo "<h4>Suma: $suma</h4>";
+            echo "<h4>Resta: $resta</h4>";
+            echo "<h4>Multiplicación: $multiplicacion</h4>";
+            echo "<h4>División: $division</h4>";
+
+            if($negrita){
+                echo "</h1>";
+            }
+        }
+
+        calculadora(10, 30, true); // Llamada a la función
+        echo "<br/><hr/>";
+
         ?>
+
+        <!-- Variable global --------------------------------------------------------------------->
+        <?php 
+            $frase = "Ni los genios son tan genios, ni los mediocres tan mediocres";
+
+            echo $frase;
+
+            function holaMundo(){
+                global $frase;
+                echo "<h1>$frase</h1>";
+
+                $year = 2019;
+                echo "<h1>$year</h1>";
+
+                return $year;
+            }
+
+            echo holaMundo();
+        ?>
+
+        <!-- Funciones variables --------------------------------------------------------------------->    
+        <?php
+                    function buenosDias(){
+                return "<h1>Hola! Buenos días :)</h1>";
+            }
+
+            function buenasTardes(){
+                return "<h1>Hey!! Qué tal ha ido la comida??</h1>";
+            }
+
+            function buenasNoches(){
+                return "<h1>¿Te vas a dormir ya? Buenas noches!!</h1>";
+            }
+
+            $horario = "Noches";
+
+            $miFuncion = "buenas".$horario;
+            echo $miFuncion();
+        ?></body>   
+        
 
 
         
